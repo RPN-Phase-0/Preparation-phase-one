0de963fd9b97aaaa583ea7071e273d98ead75983
Mygusa <175779777+Mygusa@users.noreply.github.com>
2024-08-19 09:42:57 +0700

    Quiz Rekursif is your friend

---
+++ quiz/Quiz-Rekursif-is-your-friend/soal1.js
@@ -0,0 +1,35 @@
+/*
+Diberikan sebuah function angka terbesar yang menerima satu parameter berupa array. 
+fungsi ini akan mengembalikan atau me return nilai berupa angka terbesar dari array. 
+Jika array kosong maka akan mengembalikan nilai -1
+
+RULES
+=====
+- Wajib menggunakan metode rekursif
+- Dilarang menambahkan parameter baru
+- Dilarang membuat variable di luar function palindromeRecursive
+- Dilarang mengubah tipe data parameter
+- Dilarang Menggunakan Loop
+*/
+
+function angkaTerbesar(arr) {
+  if (!arr.length) {
+    return -1;
+  }
+
+  if (arr.length === 1) {
+    return arr[0];
+  }
+
+  if (arr[0] > arr[1]) {
+    arr[1] = arr[0];
+  }
+
+  return angkaTerbesar(arr.slice(1));
+}
+
+// TEST CASES
+console.log(angkaTerbesar([2, 3, 7, 6, 5])) // 7
+console.log(angkaTerbesar([9, 3, 7, 4, 1])) // 9
+console.log(angkaTerbesar([2, 1, 7, 2, 8])) // 8
+console.log(angkaTerbesar([])) // -1
---
+++ quiz/Quiz-Rekursif-is-your-friend/soal2.js
@@ -0,0 +1,48 @@
+/**
+ * ////////////////
+ * changeXRecursive
+ * ////////////////
+ * Function ini harus dikerjakan dengan menggunakan rekursif.
+ * Function ini menerima dua parameter, yaitu:
+ *  1. data yang merupakan string dari number
+ *  2. jenis yang merupakan string berisi antara ganjil dan genap
+ *
+ * [EXAMPLE]
+ * Input: data = 012345678922468 dan jenis = ganjil
+ * Process: Karena jenisnya ganjil maka setiap bilangan genap diubah menjadi x
+ * Output: x1x3x5x7x9xxxxx
+ *
+ * [RULES]
+ * 1. Dilarang mengubah tipe parameter function
+ * 2. Dilarang membuat function diluar function yang disediakan
+ * 5. Wajib menggunakan rekursif
+ */
+
+function changeXRecursive(data, jenis) {
+  if (data.length < 1) {
+    return data;
+  }
+
+  let str = "";
+
+  if (jenis === "ganjil") {
+    if (data[0] % 2 === 0) {
+      str += "x";
+    } else {
+      str += data[0];
+    }
+  }
+
+  if (jenis === "genap") {
+    if (data[0] % 2 === 1) {
+      str += "x";
+    } else {
+      str += data[0];
+    }
+  }
+
+  return str + changeXRecursive(data.slice(1), jenis);
+}
+
+console.log(changeXRecursive("012345678922468", "ganjil")); //x1x3x5x7x9xxxxx
+console.log(changeXRecursive("0123456789", "genap")); //0x2x4x6x8x
